a8997963a8ab112f2e8c39207bff5f8f586361f2
Mattschoe <matthias.s.nielsen@gmail.com>
2021-04-13 13:50:43 +0200

    Addede nye filer

---
+++ Aersproeve/CanvasTest/canvas.js
@@ -0,0 +1,48 @@
+/* Skilpadde variables */
+var tortoiseSpeed = 1;
+var tortoisePos = 0;
+/* Skilpadde variables */
+
+/* Hare variables */
+var hareSpeed = 2;
+var harePos = 0;
+/* Søvn variables */
+var minsBetweenNaps = 30;
+var napLength = 20;
+var minsSinceLastNap = 0;
+var minsSinceNapStart = 0;
+/* Søvn variables */
+/* Hare variables */
+
+/* Generic variables*/
+var raceDistance = 350;
+/* Generic variables*/
+
+/* Haresøvn*/
+while (tortoisePos < raceDistance && harePos < raceDistance) {
+    tortoisePos += tortoiseSpeed;
+    if (minsSinceLastNap <= minsBetweenNaps) {
+        harePos += hareSpeed;
+        minsSinceLastNap++;
+    } else {
+        if (minsSinceNapStart < napLength) {
+            minsSinceNapStart++;
+        } else {
+            minsSinceLastNap = 0;
+            minsSinceNapStart = 0;
+        }
+    }
+}
+/* Haresøvn*/
+
+console.log('Tortoise position: ' + tortoisePos);
+console.log('Hare position: ' + harePos);
+
+/*Canvas pictures*/
+window.onload = function () {
+    var canvas = document.getElementById("viewport");
+    var ctx = canvas.getContext("2d");
+    var img = document.getElementById("Skildpadde.png");
+    ctx.drawImage(img, 10, 10);
+};
+/*Canvas pictures*/
